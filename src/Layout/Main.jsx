86d67e2b0a03e34bf3c032a59fbf34021dd8026d
tuhin92 <tuhinalmamun71m@gmail.com>
2024-06-30 18:11:33 +0600

    addes navbar and footer

--- src/Layout/Main.jsx
+++ src/Layout/Main.jsx
@@ -1,9 +1,13 @@
 import { Outlet } from "react-router-dom";
+import Footer from "../Pages/Shared/Footer/Footer";
+import NavBar from "../Pages/Shared/NavBar/NavBar";
 
 const Main = () => {
     return (
         <div>
+            <NavBar></NavBar>
             <Outlet></Outlet>
+            <Footer></Footer>
         </div>
     );
 };
---
+++ src/Pages/Shared/Footer/Footer.jsx
@@ -0,0 +1,37 @@
+import logo from '../../../assets/logo.svg';
+const Footer = () => {
+  return (
+    <footer className="footer bg-base-200 text-base-content p-10">
+      <aside>
+        <img src={logo} alt="" />
+        <p>
+          ACME Industries Ltd.
+          <br />
+          Providing reliable tech since 1992
+        </p>
+      </aside>
+      <nav>
+        <h6 className="footer-title">Services</h6>
+        <a className="link link-hover">Branding</a>
+        <a className="link link-hover">Design</a>
+        <a className="link link-hover">Marketing</a>
+        <a className="link link-hover">Advertisement</a>
+      </nav>
+      <nav>
+        <h6 className="footer-title">Company</h6>
+        <a className="link link-hover">About us</a>
+        <a className="link link-hover">Contact</a>
+        <a className="link link-hover">Jobs</a>
+        <a className="link link-hover">Press kit</a>
+      </nav>
+      <nav>
+        <h6 className="footer-title">Legal</h6>
+        <a className="link link-hover">Terms of use</a>
+        <a className="link link-hover">Privacy policy</a>
+        <a className="link link-hover">Cookie policy</a>
+      </nav>
+    </footer>
+  );
+};
+
+export default Footer;
